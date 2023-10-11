98ce05b0e2ad067cca277adb3b785812bb5592fd
yyjkm <yjgo0424@naver.com>
2023-10-11 16:56:48 +0900

    Week6. 배열

--- challenge/week6/studentGrade.c
+++ challenge/week6/studentGrade.c
@@ -27,19 +27,22 @@ void classifyStudents(int scores[], char targetGrade) {
 }  // 함수 classifyStudnets 원형 선언
 
 int sumScores(int scores[]) {
-	int i = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
-	return i;
+	int i = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];  // scores[0] 값부터 scores[5] 값까지 더한 값을 i 에 저장
+	return i;  // i 값 반환
 }
 
 double averageScores(int scores[]) {
 	double ave;
-	int i = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
-	ave = i / STUDENTS;
-	return ave;
-}
+	double i = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
+	ave = i / STUDENTS;  // 총 합 i를 STUDENTS 로 나누어 평균 값을 ave에 저장
+	return ave;  // ave 값 반환
+}  // 
 
 void printRanks(int scores[]) {
-
+	int Rank = 5;
+	for (int i = 0; i < STUDENTS; i++) {
+		printf("%d 학생의 순위는 %d입니다", i + 1,Rank);
+	}
 }
 
 int main() {
@@ -58,8 +61,11 @@ int main() {
 
 	classifyStudents(scores, target);
 
-	int sum = sumScores(scores);
-	double average = averageScores(scores);
-	printf("%d, %lf \n", sum, average);
+	int sum = sumScores(scores);  // 총합을 sum 에 저장
+	double average = averageScores(scores);  // 평균을 average에 저장
+	printf("학생들 점수의 총 합은 %d 이고, 평균 값은 %lf입니다. \n", sum, average);
+
+	printRanks(scores);
+
 	return 0;
 }
