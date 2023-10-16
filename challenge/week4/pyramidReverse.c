b6e7459c517892389baef9df9b8d60e6556c8e71
yyjkm <yjgo0424@naver.com>
2023-10-16 20:38:31 +0900

    Week4.2. 역피라미드 쌓기

--- challenge/week4/pyramidReverse.c
+++ challenge/week4/pyramidReverse.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int floor;
+	int floor;  // 정수형 변수 floor 선언
 	printf("몇 층을 쌓겠습니까? (5~100) \n");
-	scanf_s("%d", &floor);
+	scanf_s("%d", &floor);  // 사용자로부터 floor 변수에 값 입력받기
 
 	for (int i = floor; i > 0; i--) {
 		for (int j = 0; j < floor - i; j++) {
 			printf("S");
-		}
+		}  //  j 값이 floor - i 일때 S 출력 반복
 		for (int k = 0; k < i * 2 - 1; k++) {
 			printf("*");
-		}
+		}  //  k 값이 i * 2 -1 일때 *출력 반복
 		printf("\n");
-	}
+	}  //  i 값이 양수일 때 반복
 }
